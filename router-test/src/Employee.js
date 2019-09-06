f9394b743b494558720ba1e485d3d4d3066e0fb8
Dil Sombayeva <dilyasom@gmail.com>
2019-09-05 21:36:24 -0700

    employee API

---
+++ router-test/src/Employee.js
@@ -0,0 +1,23 @@
+const employees = [
+ {
+  id: '1',
+  name: 'Jow',
+  position: 'Software Engineer',
+ },
+ {
+  id: '2',
+  name: 'Jane',
+  position: 'PM',
+ },
+ {
+  id: '3',
+  name: 'John',
+  position: 'CEO',
+ },
+];
+
+export const getAllEmployees = () => {
+ return employees;
+};
+
+// export const
